f35333d00e6254732ba00ed6836e0ce91da1847e
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-05-05 22:22:11 +0900

    add Question4 logic

--- Javascript/libs/Question4.js
+++ Javascript/libs/Question4.js
@@ -3,14 +3,22 @@ function nearestChapter(obj, num){
     let val;
     let result;
     let key;
-    for(let i=0; i<Object.keys(obj).length; i++){
-        val =obj[Object.keys(obj)[i]];
-        if(i==0) 
-            result= Math.abs(num-val);
+    let retKey;
+    // for(let i=0; i<Object.keys(obj).length; i++){
+    //     val =obj[Object.keys(obj)[i]];
+    //     if(i==0) 
+    //         result= Math.abs(num-val);
+    //     if(result>=Math.abs(num-val))
+    //         key = Object.keys(obj)[i];
+    // }
+    for(key in obj){
+        val =obj[key];
+        if(!result){
+            result= Math.abs(num-val); }
         if(result>=Math.abs(num-val))
-            key = Object.keys(obj)[i];
+            retKey = key;
     }
-    return key;
+    return retKey;
+    
 }
-
 module.exports = nearestChapter;
