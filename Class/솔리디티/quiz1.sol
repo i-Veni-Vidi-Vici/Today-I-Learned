a372d70f5f48ccae4eee827b60c17cc56f822570
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-05-09 22:30:03 +0900

    add quiz1.sol change mock.test.js

---
+++ Class/솔리디티/quiz1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract quiz1{
+
+    function quiz11(int a) public pure returns(bool){
+        if(a>10) return true;
+        else return false;
+    }
+}
