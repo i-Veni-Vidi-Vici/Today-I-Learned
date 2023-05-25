625a7d6bb1f6c843120833e4bd2948b059021cc1
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-05-26 00:36:16 +0900

    add

--- __test__/hello.test.js
+++ __test__/hello.test.js
@@ -1,17 +1,15 @@
 const request = require("supertest")
 const {app, server} = require("../libs/app")
 
-descibe("app", ()=>{
+describe("app", ()=>{
     afterAll(()=> server.close())
-    test("hello", (done)=>{
-        request(app).get('/hello').expect(200).end((err, res)=>{
-            if(err) throw err
-            expect(res.body).toMatchObject({
-                result:true,
-                message: "hi"
-            })
+    test("hello", async()=>{
+          await request(app).get('/hello').expect(200, {
+            result:true,
+            message: "hi"
+           
         })
-        done();
+       
 
     })
 })
