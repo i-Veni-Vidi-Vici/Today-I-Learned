4811f58f37c76eab837b597bde0399ef66109bb4
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-05-29 20:34:22 +0900

    change Question5 test

--- __test__/Question5.test.js
+++ __test__/Question5.test.js
@@ -53,18 +53,12 @@ describe(`Question5`, ()=>{
       })
 
       test(`countNumberOfOccurrences({
-        a: "moron",
-        b: "scumbag",
-        c: "moron",
-        d: "idiot",
-        e: "idiot"
-      }) ➞ { moron: 2, scumbag: 1, idiot: 2 }`, ()=>{
+        a: "idiot",
+        b: "scumbag"
+      }) ➞ { idiot: 1, scumbag: 1 }`, ()=>{
         expect(countNumberOfOccurrences({
-            a: "moron",
-            b: "scumbag",
-            c: "moron",
-            d: "idiot",
-            e: "idiot"
-          })).toEqual({ moron: 2, scumbag: 1, idiot: 2 })
+            a: "idiot",
+            b: "scumbag"
+          })).toEqual({ idiot: 1, scumbag: 1 })
       })
 })
