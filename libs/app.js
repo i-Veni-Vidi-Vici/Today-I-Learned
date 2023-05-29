702a0b0f21ef0a09db0ace80b72552f7042596f1
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-05-29 22:48:39 +0900

    add supertest

---
+++ libs/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+
+const app =express();
+
+app.get("/hello", (req, res)=>{
+    return res.json({
+        retsult: true,
+        message:"hi"
+    })
+})
+
+
+const server = app.listen(8000,()=>{
+    console.log("start with 8000");
+})
+module.exports = {app, server}
