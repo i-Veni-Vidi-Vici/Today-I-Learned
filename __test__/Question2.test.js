f0423d60d9b24b7e185ec22e70a36d8405994966
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-06-04 16:34:56 +0900

    question 1 , 2

---
+++ __test__/Question2.test.js
@@ -0,0 +1,25 @@
+// 2. 배열에 있는 true의 개수를 리턴하는 함수를 만드세요.
+// 비어있는 배열이면 0을 리턴
+// 배열 값 타입은 boolean
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+
+
+const countTrue  = require("../libs/Question2")
+
+describe(`Question2`, ()=>{
+    test(`countTrue([true, false, false, true, false]) ➞ 2`, ()=>{
+        expect(countTrue([true, false, false, true, false])).toBe(2)
+    })
+
+    test(`countTrue([false, false, false, false]) ➞ 0`, ()=>{
+        expect(countTrue([false, false, false, false])).toBe(0)
+    })
+
+    test(`countTrue([]) ➞ 0`, ()=>{
+        expect(countTrue([])).toBe(0)
+    })
+})
