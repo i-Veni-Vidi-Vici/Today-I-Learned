721c75ef84142711b151fd99d67ea1587555da4f
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-06-05 13:30:28 +0900

    c

---
+++ __test__/asynchronous.test.js
@@ -0,0 +1,33 @@
+const { fetchData, fetchDataWithError, fetchDataWithCallback } = require('../libs/fetchData')
+// function fetchData() {
+//     return new Promise((res, rej) => {
+//         res('peanut butter')
+//     })
+// }
+test('the data is peanut butter', () => {
+    // fetchData 함수 테스트 (.then 함수 이용)
+    fetchData().then(val =>{expect(val).toMatch('peanut butter')})
+})
+
+// test('the data is peanut butter', () => {
+//     // fetchData 함수 테스트 (async await 함수 이용)
+// })
+
+// test('the fetch fails with an error', async () => {
+//     // fetchDataWithError 테스트
+// })
+
+// test('the data is peanut butter with .resolves', async () => {
+//     // fetchData 함수 테스트 (.resolves 함수 이용)
+// })
+
+// test('the fetch fails with an error', async () => {
+//     // assertions 함수 이용
+//     // fetchDataWithError 함수 테스트 (.rejects 함수 이용)
+// })
+
+// test('the data is peanut butter', () => {
+//     // fetchDataWithCallback 테스트 (done 이용)
+
+//     fetchDataWithCallback(callback)
+// })
