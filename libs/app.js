3b19c2cc296789ecfd1e823ff3e55e6e47965faa
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-06-05 15:07:54 +0900

    action

---
+++ libs/app.js
@@ -0,0 +1,34 @@
+// 다음을 response하는 api server를 만들고 e2e test를 수행하는 코드를 git에 올려주세요.
+
+// [url]
+
+// GET /helloworld
+
+
+// [response]
+
+// {
+
+// id: 1,
+
+// message: ‘hello, world!’
+
+// }
+
+const express = require('express');
+const app = express();
+
+app.get('/helloworld', (req, res) => {
+  res.json({
+    id: 1,
+    message: 'hello, world!'
+  })
+});
+
+
+const server = app.listen(8000, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = {app, server}
+
