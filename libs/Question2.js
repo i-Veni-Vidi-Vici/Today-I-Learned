372ac644193d725b140118afc3ba6a35d71c1900
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-06-05 13:06:18 +0900

    Question2

---
+++ libs/Question2.js
@@ -0,0 +1,15 @@
+// 2. 배열에 있는 true의 개수를 리턴하는 함수를 만드세요.
+// 비어있는 배열이면 0을 리턴
+// 배열 값 타입은 boolean
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+
+const countTrue= (arr)=>{
+
+    return arr.filter(val=> val==true).length;
+}
+
+module.exports = countTrue;
