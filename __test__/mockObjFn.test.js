7cf4f3b1983df4efbe4857a4e0678aa059b7de65
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-06-05 13:43:09 +0900

    v

--- __test__/mockObjFn.test.js
+++ __test__/mockObjFn.test.js
@@ -1,18 +1,44 @@
+
 const { fetchTitle } = require('../libs/fetchData')
 const { ModifiedTitle } = require('../libs/modifiedTitle')
 
+// async function fetchTitle() {
+//     try {
+//         const modTitle = new ModifiedTitle()
+//         const mt = await modTitle.get()
+//         return ({
+//             title: `Welcome ${mt.title}`,
+//             userId: mt.userId
+//         })
+//     } catch (e) {
+//         throw e
+//     }
+// }
 describe('normal', () => {
-    test('normal', () => {
+    test('normal', async() => {
         // fetchTitle 함수 테스트
+        expect(await fetchTitle()).toEqual({"title": "Welcome Modified: delectus aut autem", "userId": 101})
     })
 })
 
 describe('mock', () => {
     // restore all mocks after each
+    afterEach(()=>{
+        jest.restoreAllMocks();
+    })
 
-    test('mock test', () => {
+    test('mock test', async() => {
         // ModifiedTitle.prototype.get 함수 mock
         // fetchTitle 함수 테스트
+        jest.spyOn(ModifiedTitle.prototype, "get").mockResolvedValue(        {
+            title: 'Mocked: title',
+            userId: 201
+        })
+
+        expect(await fetchTitle()).toEqual(        {
+            title: 'Welcome Mocked: title',
+            userId: 201
+        })
         // 결과물
         // {
         //     title: 'Welcome Mocked: title',
@@ -20,7 +46,9 @@ describe('mock', () => {
         // }
     })
 
-    test('normal again', () => {
+    test('normal again', async() => {
         // 원래 fetchTitle 함수 테스트
+        expect(await fetchTitle()).toEqual({"title": "Welcome Modified: delectus aut autem", "userId": 101})
+
     })
 })
